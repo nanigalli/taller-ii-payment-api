a7e72bf079021b1dae09a91fa08abdaea97cb4e3
Gabriel Fusca <gfusca@artear.com>
2017-08-10 10:47:49 -0300

    added port config to app.run

--- main.py
+++ main.py
@@ -52,5 +52,7 @@ def get_pay_methods():
   })
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9999, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
 
