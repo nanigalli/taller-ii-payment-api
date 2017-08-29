48850ca001865d91933ca5bd0f920c4658f8d7b4
Gabriel Fusca <gfusca@artear.com>
2017-08-29 18:26:13 -0300

    remove mock data and use imports from mock files

--- test/test_authorization.py
+++ test/test_authorization.py
@@ -3,25 +3,16 @@ from models.token import Token
 import mock 
 import unittest
 from flask import Flask, jsonify
+from mocks.authorization_response import authorization_response
+from mocks.token_create_mock import token_creation_mock
 
 class AuthorizationResourceTestCase(unittest.TestCase):
 
   def test_authorization(self):
-    response = {
-      "access_token": "994c1464-3f4c-4077-9253-656d32dc88fb",
-      "expires_in": "1504028964",
-      "grant_type": "client_credentials",
-      "token_type": "bearer"
-    }
     service = AuthorizationResource()
     service._get_client_id_from_request = mock.MagicMock(return_value='ee04c1bd-bd98-4ac9-861e-cff1834e0386')
     service._get_client_secret_from_request = mock.MagicMock(return_value='1e238cae-26ae-412d-a7e6-959e89980a13')
-    service._create_access_token_response = mock.MagicMock(return_value=response)
-    jsonify = mock.MagicMock(return_value=response)
-    Token.create = mock.MagicMock(
-      return_value = {
-        "access_token": "994c1464-3f4c-4077-9253-656d32dc88fb",
-        "expiration": "1504028964"
-      }
-    )
-    self.assertEqual(service.post(), response)
+    service._create_access_token_response = mock.MagicMock(return_value=authorization_response)
+    jsonify = mock.MagicMock(return_value=authorization_response)
+    Token.create = mock.MagicMock(return_value = token_creation_mock)
+    self.assertEqual(service.post(), authorization_response)
