96a1dc16c389f923aaea2787edaa1bc0360dab43
Gabriel Fusca <gfusca@artear.com>
2017-08-30 18:40:55 -0300

    create Authorization class

--- resources/auth.py
+++ resources/auth.py
@@ -5,9 +5,11 @@ class InvalidAuthTypeException(Exception):
 class InvalidTokenException(Exception):
   pass
 
-def auth(authtype, token):
-  if (authtype != "Bearer" and authtype != "bearer"):
-    raise InvalidAuthTypeException("Authorization type must be bearer!")
-  if ( not Token.exists(token)):
-    raise InvalidTokenException("Invalid authorization token!")  
+class Authorization:
 
+  @staticmethod
+  def auth(authtype, token):
+    if (authtype != "Bearer" and authtype != "bearer"):
+      raise InvalidAuthTypeException("Authorization type must be bearer!")
+    if ( not Token.exists(token)):
+      raise InvalidTokenException("Invalid authorization token!")
