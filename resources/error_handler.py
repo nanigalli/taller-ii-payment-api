ebee011b53a0143e82282992c3c7d2cebc0e9498
Gabriel Fusca <gfusca@artear.com>
2017-08-30 18:36:59 -0300

    create functions to build error and successful responses

---
+++ resources/error_handler.py
@@ -0,0 +1,14 @@
+from flask import jsonify, make_response
+
+
+def create_error_response(status_code, message):
+  return make_response(
+    jsonify(
+          {
+            "status_code": status_code,
+            "message": message
+          }
+    ),
+    status_code
+  )
+  
