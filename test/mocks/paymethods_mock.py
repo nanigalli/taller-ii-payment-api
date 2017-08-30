6652df6d4630059bf4dbc98810b6672920de799d
Gabriel Fusca <gfusca@artear.com>
2017-08-30 18:37:18 -0300

    add paymethod resource test case

---
+++ test/mocks/paymethods_mock.py
@@ -0,0 +1,14 @@
+paymethod_response_mock = {
+  "items": [
+    {
+      "paymethod": "card",
+      "parameters": {
+        "number": "string",
+        "type": "string",
+        "expirationMonth": "number",
+        "expirationYear": "number",
+        "ccvv": "number"
+      }
+    }
+  ]
+}
