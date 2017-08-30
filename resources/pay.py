a50e9b1d8bdc4946cb5840c23ff21efc8964b789
Gabriel Fusca <gfusca@artear.com>
2017-08-30 18:41:16 -0300

    update resource to use Authorization class

--- resources/pay.py
+++ resources/pay.py
@@ -1,14 +1,14 @@
 from flask_restful import Resource
 from flask import jsonify, make_response, request
 import uuid
-from auth import auth, InvalidAuthTypeException, InvalidTokenException
+from auth import Authorization, InvalidAuthTypeException, InvalidTokenException
 
 
 class Pay(Resource):
   def post(self):
     authtype, token = request.headers.get('Authorization').split()
     try:
-      auth(authtype, token)
+      Authorization.auth(authtype, token)
       transaction_id = str(uuid.uuid4())
       return make_response(
         jsonify(
